0da1f9427a7b1d83488ab200ac1592b22fe4fb4e
Harshit Jaiswal <harshit.punter19@gmail.com>
2019-11-05 00:50:12 +0530

    2ndCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ProductsAdminComponent } from './admin/products-admin/products-admin.component';
+import { OrdersAdminComponent } from './admin/orders-admin/orders-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckoutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    ProductsAdminComponent,
+    OrdersAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBHTtpGix7KOIm_8w8yFfnukMoDB951BjE",
+    authDomain: "ecommerce-shop-using-angular.firebaseapp.com",
+    databaseURL: "https://ecommerce-shop-using-angular.firebaseio.com",
+    projectId: "ecommerce-shop-using-angular",
+    storageBucket: "ecommerce-shop-using-angular.appspot.com",
+    messagingSenderId: "1004625658953",
+    appId: "1:1004625658953:web:2d3d95299827ec5a446b98",
+    measurementId: "G-HDHLVZC1YD"
+  }
 };
